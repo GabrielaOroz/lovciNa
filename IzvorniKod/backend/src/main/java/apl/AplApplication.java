d7d4c7e7ea75af4adf82b1062ca3878c5925fd7f
Mia Sara <mia-sara.matusin@fer.hr>
2023-11-01 16:26:25 +0100

    backend setup

---
+++ IzvorniKod/backend/src/main/java/apl/AplApplication.java
@@ -0,0 +1,13 @@
+package apl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplApplication.class, args);
+	}
+
+}
---
+++ IzvorniKod/backend/src/test/java/apl/AplApplicationTests.java
@@ -0,0 +1,13 @@
+package apl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
