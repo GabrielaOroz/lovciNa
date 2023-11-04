bf131e197e37014cf297cee39607287827a518e3
Gabriela Oroz <gabriela.oroz@fer.hr>
2023-11-04 17:27:11 +0100

    Connecting frontend with backend

--- IzvorniKod/backend/src/main/java/apl/rest/UserController.java
+++ IzvorniKod/backend/src/main/java/apl/rest/UserController.java
@@ -3,11 +3,16 @@ package apl.rest;
 import apl.domain.User;
 import apl.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import javax.xml.crypto.Data;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/users")
@@ -19,5 +24,12 @@ public class UserController {
     @GetMapping
     public List<User> listUsers() {
         return userService.listAll();
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> handleData(@RequestBody User data) {
+
+        System.out.println(data.toString());
+        return ResponseEntity.ok("Data received and processed");
     }
 }
