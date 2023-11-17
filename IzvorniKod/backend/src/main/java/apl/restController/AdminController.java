a5ddbbb01b86e0a59e33d019d7f99a11d1b818ea
Dorjan Štrbac <dorjan.strbac@fer.hr>
2023-11-17 05:26:45 +0100

    spremno za merge u master i kreiranje web servisa

--- IzvorniKod/backend/src/main/java/apl/restController/AdminController.java
+++ IzvorniKod/backend/src/main/java/apl/restController/AdminController.java
@@ -12,7 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.List;
 
-@CrossOrigin(origins = "https://wildtrackfront.onrender.com")
+@CrossOrigin(origins = "https://wildtrack.onrender.com")
 @RestController
 public class AdminController {
 
