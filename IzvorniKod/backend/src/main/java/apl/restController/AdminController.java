7e4c63807f91b9993d9aa44ea308cd6dfe671b10
Dorjan Štrbac <dorjan.strbac@fer.hr>
2023-11-17 04:24:07 +0100

    configured CORS on back

--- IzvorniKod/backend/src/main/java/apl/restController/AdminController.java
+++ IzvorniKod/backend/src/main/java/apl/restController/AdminController.java
@@ -12,7 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://wildtrackfront.onrender.com")
 @RestController
 public class AdminController {
 
