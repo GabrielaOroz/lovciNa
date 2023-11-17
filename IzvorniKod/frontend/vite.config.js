5775b5701586bfa16aebfde53707adff946e42e8
Dorjan Štrbac <dorjan.strbac@fer.hr>
2023-11-17 04:04:15 +0100

    added Dockerfile

--- IzvorniKod/frontend/vite.config.js
+++ IzvorniKod/frontend/vite.config.js
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: process.env.PORT || 3000,
+    host: true,
+    strictPort: true,
+    port: 8080
   },
 });
